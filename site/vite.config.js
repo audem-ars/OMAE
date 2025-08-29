10902211d858e2e5555dbcab13bec7490b77c019
Papi Gringo <papigringo@Papis-Mac-mini.local>
2025-08-29 22:47:30 +0200

    fix(vite): proper multipage entries; real get-started page; deploy to docs

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+
 export default defineConfig({
   base: './',
   build: {
     rollupOptions: {
       input: {
         index: resolve(__dirname, 'index.html'),
-        getStarted: resolve(__dirname, 'get-started.html')
+        getStarted: resolve(__dirname, 'get-started.html'),
       }
     }
   }
